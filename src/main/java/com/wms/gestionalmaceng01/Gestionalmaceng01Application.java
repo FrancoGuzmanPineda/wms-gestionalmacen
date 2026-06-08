64c5acd7350b586ad55b59b87906e208f2bf03ef
VFrank2384 <138312371+VFrank2384@users.noreply.github.com>
2026-06-07 22:52:30 -0500

    first commit

---
+++ src/main/java/com/wms/gestionalmaceng01/Gestionalmaceng01Application.java
@@ -0,0 +1,13 @@
+package com.wms.gestionalmaceng01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gestionalmaceng01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gestionalmaceng01Application.class, args);
+	}
+
+}
