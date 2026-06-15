55f5c180ef87314b356ab7d752174e0a683170bd
Renzo Rodriguez <aguila360ss@gmail.com>
2026-06-15 13:57:38 -0500

    Implementacion modulo de reportes WMS

--- src/main/java/com/wms/gestionalmaceng01/repository/MovimientoRepository.java
+++ src/main/java/com/wms/gestionalmaceng01/repository/MovimientoRepository.java
@@ -2,6 +2,9 @@ package com.wms.gestionalmaceng01.repository;
 
 import com.wms.gestionalmaceng01.models.Movimiento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 import java.util.List;
 
 public interface MovimientoRepository extends JpaRepository<Movimiento, Integer> {
@@ -11,5 +14,31 @@ public interface MovimientoRepository extends JpaRepository<Movimiento, Integer>
     List<Movimiento> findByTipoMovimiento(String tipoMovimiento);
 
     List<Movimiento> findByProductoIdProducto(Integer idProducto);
+
+    @Query("""
+SELECT m
+FROM Movimiento m
+WHERE m.tipoMovimiento='Ingreso'
+AND DATE(m.fecha)
+BETWEEN :inicio AND :fin
+ORDER BY m.fecha DESC
+""")
+List<Movimiento> buscarEntradasPorFecha(
+        LocalDate inicio,
+        LocalDate fin
+);
+
+@Query("""
+SELECT m
+FROM Movimiento m
+WHERE m.tipoMovimiento='Salida'
+AND DATE(m.fecha)
+BETWEEN :inicio AND :fin
+ORDER BY m.fecha DESC
+""")
+List<Movimiento> buscarSalidasPorFecha(
+        LocalDate inicio,
+        LocalDate fin
+);
 
 }
